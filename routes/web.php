e9348906ccf014643c2e535c5c229f1f62204d53
Arthur-Coppey <arthur.coppey@viacesi.fr>
2021-08-27 17:54:09 +0200

    named redirect

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('fullcalendar.index');
 });
 
 Route::prefix('fullcalendar')->name('fullcalendar.')->group(function () {
